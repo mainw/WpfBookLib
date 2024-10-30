94b68676faf0356cebfc5dd7dc70ef5d4c3f8b7c
mainw <133776268+mainw@users.noreply.github.com>
2024-10-30 11:19:27 +0300

    Добавление контекста

--- DbApi/DbBookLib/Db.cs
+++ DbApi/DbBookLib/Db.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using ModelsLib.Models;
 namespace DbApi.DbBookLib
 {
     public class Db : DbContext
     {
         public Db() { }
-
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookFeedbacks> BookFeedbacks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<GenresEnum> GenresEnum { get; set; }
+        public DbSet<MarkEnum> MarkEnum { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBookInteraction> UserBookInteractions { get; set; }
     }
 }
